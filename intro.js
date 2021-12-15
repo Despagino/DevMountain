06769977bc5fa6d5fd79b9b140a814ea4007ec35
Gino Tasis <gtasis84@gmail.com>
2021-12-15 12:59:25 -0500

    better solution for finding largest and smallest of an array

--- intro.js
+++ intro.js
@@ -2,11 +2,7 @@
 
 var array = [1, 4, 11, 2, 37, -4]
 
-for (let i = 0; i < array.length; i++) {
- 
-        let largest = Math.max(array)
-        let smallest = Math.min(array)
-    console.log(largest, smallest)
+console.log(`Largest number: ${Math.max(...array)}, Smallest number: ${Math.min(...array)}`)
 
-}
+
 
