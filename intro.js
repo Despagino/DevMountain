7a6fb491e6e101bfcdf224500e1a19af2a3b01a8
Gino Tasis <gtasis84@gmail.com>
2021-12-15 13:05:24 -0500

    added for loop solution

--- intro.js
+++ intro.js
@@ -2,7 +2,23 @@
 
 var array = [1, 4, 11, 2, 37, -4]
 
-console.log(`Largest number: ${Math.max(...array)}, Smallest number: ${Math.min(...array)}`)
+// oneliner
+// console.log(`Largest number: ${Math.max(...array)}, Smallest number: ${Math.min(...array)}`)
 
 
 
+// for loop version
+
+let smallest = array[0]
+let largest = array[0]
+
+for (let i = 0; i < array.length; i++) {
+    if (smallest > array[i]) {
+        smallest = array[i]
+        
+    } else if (largest < array[i]) {
+        largest = array[i]
+        
+    }
+}
+console.log(smallest, largest)
