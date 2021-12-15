8818349dab1d6f54faae6f5dfe82d58bf73ca01f
Gino Tasis <gtasis84@gmail.com>
2021-12-15 14:34:45 -0500

    added logic, finished vars demo

--- making-decisions-demo/varsAndConditionals.js
+++ making-decisions-demo/varsAndConditionals.js
@@ -3,3 +3,23 @@
     and loops to play out a fight between Jon Snow and
     Jamie Lannister.
 */ 
+
+
+let jonSnowAttack = 25
+let jamieLannisterAttack =35
+let jonSnowHealth = 50
+
+if (jamieLannisterAttack > jonSnowAttack) {
+    console.log("Jamie has a better attack")
+} else if (jonSnowAttack > jamieLannisterAttack) {
+    console.log("Jon has a better attack")
+} else {
+    console.log("Jon and Jami have the same attack")
+}
+
+if (jonSnowHealth <= 0) {
+    console.log("Jon Snow is dead")
+} else {
+    jonSnowHealth -= jamieLannisterAttack
+}
+console.log(jonSnowHealth)
